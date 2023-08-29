5536035044fe67b948fcf6239bbebc8cef131fd7
chimkun <stevenhoangama@gmail.com>
2023-08-29 15:52:39 +0700

    server UI

--- mainServer.py
+++ mainServer.py
@@ -21,11 +21,13 @@ PROCESS_MSG = "!PROCESS"
 
 connected = False
 IP = ""
+thread = threading.Thread(target = sc.start)
 
 def launch(textLabel):
     IP = sc.socket.gethostbyname(sc.socket.gethostname())
-    threading.Thread(target = sc.start()).start()
+    thread.start()
     textLabel.config(text = "Server IP: " + IP)
+    textLabel.grid(row = 1, column = 1, columnspan = 7, sticky = "w")
 
 # def openAppRunningWindow():
 #     popup = tk.Toplevel()
@@ -38,35 +40,37 @@ def launch(textLabel):
 root = tk.Tk()
 root.title("Client")
 
-window_width = 200
-window_height = 150 
+window_width = 195
+window_height = 120
 
 root.geometry(f"{window_width}x{window_height}")
 
 labelString = ""    
 
-emptyLabel_a0 = tk.Label(root, height = 1, text = labelString)
+emptyLabel_a0 = tk.Label(root, height = 1, width = 2, text = labelString)
 emptyLabel_a0.grid(row = 0, column = 0)
-emptyLabel_a1 = tk.Label(root, height = 1, text = labelString)
+emptyLabel_a1 = tk.Label(root, height = 1, width = 2, text = labelString)
 emptyLabel_a1.grid(row = 0, column = 1)
-emptyLabel_a2 = tk.Label(root, height = 1, text = labelString)
+emptyLabel_a2 = tk.Label(root, height = 1, width = 2, text = labelString)
 emptyLabel_a2.grid(row = 0, column = 2)
-emptyLabel_a3 = tk.Label(root, height = 1, text = labelString)
+emptyLabel_a3 = tk.Label(root, height = 1, width = 2, text = labelString)
 emptyLabel_a3.grid(row = 0, column = 3)
-emptyLabel_a4 = tk.Label(root, height = 1, text = labelString)
+emptyLabel_a4 = tk.Label(root, height = 1, width = 2, text = labelString)
 emptyLabel_a4.grid(row = 0, column = 4)
-emptyLabel_a5 = tk.Label(root, height = 1, text = labelString)
+emptyLabel_a5 = tk.Label(root, height = 1, width = 2, text = labelString)
 emptyLabel_a5.grid(row = 0, column = 5)
-emptyLabel_a6 = tk.Label(root, height = 1, text = labelString)
+emptyLabel_a6 = tk.Label(root, height = 1, width = 2, text = labelString)
 emptyLabel_a6.grid(row = 0, column = 6)
-emptyLabel_a7 = tk.Label(root, height = 1, text = labelString)
+emptyLabel_a7 = tk.Label(root, height = 1, width = 2, text = labelString)
 emptyLabel_a7.grid(row = 0, column = 7)
 
 textLabel = tk.Label(root, text = "Server IP:")
-textLabel.grid(row = 1, column = 1)
+
+emptyLabel_1 = tk.Label(root, width = 3)
+emptyLabel_1.grid(row = 1, column = 0)
 
 serverButton = tk.Button(root, text = "Launch server", command = partial(launch, textLabel), height = 2)
-serverButton.grid(row = 2, column = 1, rowspan = 2, columnspan = 2, sticky = "ew")
+serverButton.grid(row = 2, column = 1, rowspan = 2, columnspan = 7, sticky = "ew", padx = 2)
 
 
 root.mainloop()
